daa9cfeee324c4aded9c346fd722661ed2965777
fosa <fosahadal@protonmail.com>
2024-06-13 17:24:06 -0600

    auth clerk/convex

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+
+const authConfig = {
+    providers: [
+      {
+        domain: "https://supreme-cougar-98.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+  export default authConfig;
