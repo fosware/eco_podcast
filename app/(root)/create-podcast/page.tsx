b7573144a038f8cb0401430ff5c34e457cc98a3d
fosa <fosahadal@protonmail.com>
2024-07-12 15:52:16 -0600

    add openai

--- app/(root)/create-podcast/page.tsx
+++ app/(root)/create-podcast/page.tsx
@@ -26,24 +26,40 @@ import {
 import { cn } from "@/lib/utils"
 import { useState } from "react"
 import { Textarea } from "@/components/ui/textarea"
+import GeneratePodcast from "@/components/GeneratePodcast"
+import GenerateThumbnail from "@/components/GenerateThumbnail"
+import { Loader } from "lucide-react"
+import { Id } from "@/convex/_generated/dataModel"
 
 const voiceCategories = ['alloy', 'shimmer', 'nova', 'echo', 'fable', 'onyx'];
 
 const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
+  podcastTitle: z.string().min(2),
+  podcastDescription: z.string().min(2),
 })
 
 const CreatePodcast = () => {
+  const [imagePropmpt, setImagePropmpt] = useState('');
+  const [imageStorageId, setImageStorageId] = useState<Id<"_storage"> | null>( null);
+  const [imageUrl, setImageUrl] = useState('');
 
+  
+  const [audioUrl, setAudioUrl] = useState('');
+  const [audioStorageId, setAudioStorageId] = useState<Id<"_storage"> | null>( null);
+  const [audioDuration, setAudioDuration] = useState(0);
+  
   const [voiceType, setVoiceType] = useState<string | null>(null)
+  const [voicePrompt, setVoicePrompt] = useState('');
+  
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
 
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "",
+      podcastTitle: "",
+      podcastDescription: ""
     },
   })
 
@@ -65,25 +81,23 @@ const CreatePodcast = () => {
               name="podcastTitle"
               render={({ field }) => (
                 <FormItem className="flex flex-col gap-2.5">
-                  <FormLabel className="text-16 font-bold text-white-1">Podcast title</FormLabel>
+                  <FormLabel className="text-16 font-bold text-white-1">Title</FormLabel>
                   <FormControl>
-                    <Input className="input-class focus-visible:ring-orange-1" placeholder="Eco Podcast" {...field} />
+                    <Input className="input-class focus-visible:ring-offset-orange-1" placeholder="Eco Podcast" {...field} />
                   </FormControl>
-
                   <FormMessage className="text-white-1" />
                 </FormItem>
               )}
-
             />
             <div className="flex flex-col gap-2.5">
               <Label className="text-16 font-bold text-white-1">
                 Selec AI voice
               </Label>
               <Select onValueChange={(value) => setVoiceType(value)}>
-                <SelectTrigger className={cn('text-16 w-full border-none text-gray-1')}>
+                <SelectTrigger className={cn('text-16 w-full border-none text-gray-1 focus-visible:ring-offset-orange-1')}>
                   <SelectValue placeholder="Select AI voice" className="placeholder:text-gray-1" />
                 </SelectTrigger>
-                <SelectContent className="text-16 border-none bg-black-1 font-bold text-white-1 focus:ring-orange-1">
+                <SelectContent className="text-16 border-none bg-black-1 font-bold text-white-1 focus-visible:ring-offset-orange-1">
                   {voiceCategories.map((category) => (
                     <SelectItem key={category} value={category} className="capitalize focus:bg-orange-1">
                       {category}
@@ -106,17 +120,38 @@ const CreatePodcast = () => {
                 <FormItem className="flex flex-col gap-2.5">
                   <FormLabel className="text-16 font-bold text-white-1">Description</FormLabel>
                   <FormControl>
-                    <Textarea className="input-class focus-visible:ring-orange-1" placeholder="Write a short podcast description" {...field} />
+                    <Textarea className="input-class focus-visible:ring-offset-orange-1" placeholder="Write a short podcast description" {...field} />
                   </FormControl>
-
                   <FormMessage className="text-white-1" />
                 </FormItem>
               )}
-
             />
           </div>
-          <div className="flex lex-col pt-10">
+          <div className="flex flex-col pt-10">
+            <GeneratePodcast 
+              setAudioStorageId={setAudioStorageId}
+              setAudio={setAudioUrl}
+              voiceType={voiceType!}              
+              audio={audioUrl}
+              voicePrompt={voicePrompt}
+              setVoicePrompt={setVoicePrompt}
+              setAudioDuration={setAudioDuration}
+            />
+            <GenerateThumbnail />
 
+            <div className="mt-10 w-full border border-orange-1">
+              <Button type="submit" className="text-16 w-full bg-orange-1 py-4 font-extrabold
+              text-white-1 transition-all duration-500 hover:bg-black-1">
+                {isSubmitting ? (
+                  <>
+                    Submitting
+                    <Loader size={20} className="animate-spin ml-2" />
+                  </>
+                ) : (
+                  'Submitting & Publish Podcast'
+                )}
+              </Button>
+            </div>
           </div>
         </form>
       </Form>
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,8 @@
 import LeftSidebar from '@/components/LeftSidebar';
-import Image from "next/image";
-import RightSidebar from '@/components/RightSidebar';
 import MobileNav from '../../components/MobileNav';
+import RightSidebar from '@/components/RightSidebar';
+import Image from "next/image";
+import { Toaster } from "@/components/ui/toaster";
 
 export default function RootLayout({
   children,
@@ -24,7 +25,7 @@ export default function RootLayout({
                   <MobileNav />
                 </div>
                 <div className="flex flex-col md:pb-14">
-                  Toaster
+                  <Toaster />
                   {children}
                 </div>
               </div>
